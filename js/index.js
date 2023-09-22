a29cbac7568bdffe51a7d874e58903a1582b21c6
MarIb <martaa.ibarra@hotmail.com>
2023-09-22 12:47:36 +0200

    Add first function getAnimalSpecies

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const animalA = {
+  name: "Woolly mammoth",
+  age: 8,
+  weight: 4000,
+  species: "Elephas primigenius",
+  exctionctionDate: -2000,
+};
+const animalB = {
+  name: "Pteranodon",
+  age: 4,
+  wight: 50,
+  species: "Pteranodon longiceps",
+  exctincitonDate: -100000,
+};
+
+const getAnimalSpecies = (animal) => {
+  return animal.species;
+};
+
+// refactorizado: const getAnimalSpecies = (animal) => animal.species
+
